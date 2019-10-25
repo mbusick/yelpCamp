9669174c786ed76161ca8c5fd42131552bd0eb60
mbusick <mbusick89@gmail.com>
2019-10-25 12:15:30 -0400

    changed port

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, process.env.IP, function(){
+app.listen(5000, process.env.IP, function(){
 	console.log("The YelpCamp Server Has Started!");
 });
 
